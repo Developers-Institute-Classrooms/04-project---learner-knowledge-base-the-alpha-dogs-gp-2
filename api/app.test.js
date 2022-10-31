d9e1516bad5dd1b3f84094c88aabe856c64c9cca
Guy <guykibby@gmail.com>
2022-10-31 13:54:31 +1300

    Update app.test.js
    Co-Authored-By: Shan <52937130+shvinprog@users.noreply.github.com>
    Co-Authored-By: olle1134 <99058610+olle1134@users.noreply.github.com>
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>

--- api/app.test.js
+++ api/app.test.js
@@ -3,17 +3,7 @@ const app = require("./app");
 
 describe("Q&A API", () => {
   test("GET /getData should return list of questions and answers with status code of 200", async () => {
-    const response = await request(app).get("/getData");
-    expect(response.body).toEqual([
-      {
-        id: 1,
-        question_description: "what is string interpolation?",
-      },
-      {
-        id: 2,
-        question_description: "what is HTML?",
-      },
-    ]);
+    expect(true).toEqual(false);
     expect(response.status).toBe(200);
   });
 });
