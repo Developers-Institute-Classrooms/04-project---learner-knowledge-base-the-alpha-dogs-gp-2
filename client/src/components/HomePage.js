5b46d34b5d84e03b2ee58bf2ffafac47cabaf194
Guy <guykibby@gmail.com>
2022-11-09 13:06:37 +1300

    passing props between routes

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,12 +1,18 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-const HomePage = () => {
+const HomePage = ({ setQuestionObject }) => {
+  const navigate = useNavigate(10);
+  const e = 5000;
+
+  const handleClick = (choice) => {
+    setQuestionObject(5000);
+    navigate(`/questions/${e / 2}`);
+  };
+
   return (
-    <div>
-      <Link to={"/questions/" + 1} className="list-item">
-        View Questions
-      </Link>
-    </div>
+    <button value={e} onClick={handleClick} className="list-item">
+      {e}
+    </button>
   );
 };
 
