d6d187aa0e4064f07838468583b7ae05d3430c48
Akshay Nalawade <akshay.nalawade@developersinstitute.co.nz>
2022-11-17 04:04:37 +1300

    cleared console.logs and unwanted comments

--- api/routes/usersRouter/users.router.js
+++ api/routes/usersRouter/users.router.js
@@ -14,34 +14,23 @@ router.get("/", async (request, response, next) => {
         .status(400)
         .json({ message: "Please enter a valid email and password." });
     }
-
     const existingEmail = await repository.checkEmail(email);
-
     if (existingEmail.length === 0) {
       const error = new Error("Email doesnt exist");
       error.status = 404;
       throw error;
     }
-
     const result = await repository.getUser(email);
-
     const { passwordkey, name, id, scope } = result;
-
     const authenticationStatus = await bcrypt.compare(password, passwordkey);
-    
-
     if (!authenticationStatus) {
       const error = new Error("Invalid Password");
       error.status = 400;
       throw error;
     }
-    // console.log(result);
-
     const token = jwt.sign({ id: id }, config.secret, {
       expiresIn: 1200,
     });
-    // console.log(token);
-
     return response.json({
       token,
       user: {
@@ -50,8 +39,6 @@ router.get("/", async (request, response, next) => {
         scope,
       },
     });
-    // }
-    // response.status(200).send({ message: "WOOOOHOOOOOOO" });
   } catch (error) {
     next(error);
   }
@@ -60,41 +47,14 @@ router.get("/", async (request, response, next) => {
 router.post("/", async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
-    console.log(name);
-    console.log(email);
-    console.log(password);
-
     const existingEmail = await repository.checkEmail(email);
-
     if (existingEmail.length > 0) {
       const error = new Error("Email already exists");
       error.status = 400;
       throw error;
     }
-
-    //
-
-    //create salt and hash to encrypt the password
     const hashedPassword = bcrypt.hashSync(password, 12);
-    console.log(hashedPassword);
-
-    const result = await repository.postNewUser(name, email, hashedPassword);
-    // console.log(result);
-    // newUser.password = hashedPassword;
-    // const savedUser = await newUser.save();
-    // // create a JWT token
-    // const token = jwt.sign({ id: savedUser._id }, config.jwtsecret, {
-    //   expiresIn: 3600,
-    // });
-    // console.log(token);
-    // return response.status(200).json({
-    //   token,
-    //   user: {
-    //     name: savedUser.name,
-    //     email: savedUser.email,
-    //     id: savedUser._id,
-    //   },
-    // });
+    await repository.postNewUser(name, email, hashedPassword);
     res.status(200).send({ message: "WOOOOHOOOOOOO" });
   } catch (error) {
     next(error);
